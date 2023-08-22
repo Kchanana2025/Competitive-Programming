1660b7a013157ecae0c2be8760e5d6005891e7c6
Kchanana2025 <kanikachanana2025@gmail.com>
2023-08-22 10:28:17 +0530

    Create Hard_compare.cpp

---
+++ week1/Hard_compare.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c, d;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    if (pow(a, b) > pow(c, d))
+        cout << "YES";
+    else
+        "NO";
+    return 0;
+}
